dd9182e3badf803aedc4ece7c6fcf40b651d3724
Francis Onyido <frankloginss@gmail.com>
2022-10-19 20:08:01 -0500

    Added github actions and ssh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import cors from "cors"
+import path from 'path'
 // import bodyParser from "body-parser"
 
 const app = express()
+// const path = require('path')
 
+app.use(express.static(path.join(__dirname, '/taskmgr-app/build')))
 app.use(cors())
 app.use(express.json())
 
