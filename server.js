1b6fda5309e98be7795039f799d8d7fbfbc34bac
Francis Onyido <frankloginss@gmail.com>
2022-10-21 13:37:26 -0500

    Modified app files

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
-// import path from 'path'
+var path_1 = __importDefault(require("path"));
 // import bodyParser from "body-parser"
 var app = express_1.default();
 // const path = require('path')
-// app.use(express.static(path.join(__dirname, '/taskmgr-app/build')))
+app.use(express_1.default.static(path_1.default.join(__dirname, '/taskmgr-app/build')));
 app.use(cors_1.default());
 app.use(express_1.default.json());
 var port = 4000;
